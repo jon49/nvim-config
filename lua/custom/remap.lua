1bf2b9c6140436818008efe255e5161e3f74e446
Jon Nyman <nymanjon@gmail.com>
2023-05-30 15:46:43 -0700

    New mappings jj escape & Q write & quite

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -9,8 +9,10 @@ map.set("n", "-", vim.cmd.Ex, { desc = "Open file explorer." })
 -- map.set("n", "<leader>E", ":badd<CR>:Ex<CR>", { desc = "Open file explorer in new buffer." }) This doesn't seem to work?!?!?
 map.set("n", "<leader>c", ":bd<CR>", { desc = "Close current buffer." })
 map.set("n", "<leader>E", "<C-6>", { desc = "Open file explorer in new buffer." })
+map.set("n", "Q", ":wq<CR>", { desc = "Write & quit." })
 map.set("n", "q", ":q<CR>", { desc = "Quit." })
 map.set("n", "<leader>w", ":w<CR>", { desc = "Save." })
+map.set("n", "jj", "<ESC>", { desc = "Escape." })
 
 map.set("n", "<leader>p", '"+p', { desc = "Paste after from clipboard." })
 map.set("n", "<leader>P", '"+P', { desc = "Paste before from clipboard." })
