2d8eda6648cecea135f4d285eda9313c4ac7b7b5
Jon Nyman <nymanjon@gmail.com>
2023-05-30 15:47:01 -0700

    make lsp mapping make more sense.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -65,7 +65,7 @@ lsp.on_attach(function(client, bufnr)
   end
 
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+  vim.keymap.set("n", "gh", vim.lsp.buf.hover, opts)
   vim.keymap.set(
     "n",
     "<leader>e",
@@ -76,8 +76,8 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
   vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
   vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-  vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-  vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+  vim.keymap.set("n", "grf", vim.lsp.buf.references, opts)
+  vim.keymap.set("n", "grn", vim.lsp.buf.rename, opts)
   vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
 end)
 
