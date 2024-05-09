b42d6aade7fe9f2c9893712e4b1a2df85bb6b75d
Jon Nyman <nymanjon@gmail.com>
2024-05-09 13:32:15 -0700

    Add enter as a possible selection for LSP.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -76,6 +76,7 @@ cmp.setup({
         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<Enter>'] = cmp.mapping.confirm({ select = true }),
         ["<C-Space>"] = cmp.mapping.complete(),
         -- disable completion with tab
         -- this helps with copilot setup
